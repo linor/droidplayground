e89f1b845a64695e25f1244410de02436645b8cd
Jeroen Stroeve <jeroen@stroevesoer.nl>
2026-07-12 18:41:15 +0200

    Switch to DCMotorCfg

--- rl/source/droidplayground/droidplayground/assets/qmini.py
+++ rl/source/droidplayground/droidplayground/assets/qmini.py
@@ -2,7 +2,7 @@
 
 
 import isaaclab.sim as sim_utils
-from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.actuators import DCMotorCfg
 from isaaclab.assets import ArticulationCfg
 from droidplayground.assets import ASSET_USD_DIRECTORY
 
@@ -32,17 +32,56 @@ QMINI_CFG = ArticulationCfg(
         pos=(0.0, 0.0, 2.0), joint_pos={".*": 0.0}
     ),
     actuators={
-        "xl330_velocity_actuator": ImplicitActuatorCfg(
-            joint_names_expr=[".*"],
+        # "xl330_velocity_actuator": DCMotorCfg(
+        #     joint_names_expr=[".*"],
 
-            # XL330-M288-T @ 5V
-            #effort_limit_sim=0.52,          # Nm, stall torque
-            effort_limit_sim=2.0,          
-            velocity_limit_sim=10.8,        # rad/s, 103 rpm
+        #     # effort_limit=23.7,          # continuous output torque
+        #     # The datasheet advertises maximum torque, not continuous torque. Looking at the specifications:
+        #     # A more conservative simulation would use something like
+        #     effort_limit=16.0,   # or 18 Nm
 
-            # velocity drive: stiffness = 0, damping = velocity gain
-            stiffness=50.0,
-            damping=0.0,
+        #     saturation_effort=23.7,     # peak torque used for saturation model
+        #     velocity_limit=30.0,        # rad/s
+
+        #     # velocity drive: stiffness = 0, damping = velocity gain
+        #     stiffness=60.0,      # tune
+        #     damping=1.5,         # tune
+        # )
+
+        # For the GO-M8010-6 with its 6.33:1 reduction, I'd start with:
+        # armature = 0.002  # kg·m²
+
+        # If the joint oscillates, increase damping first. If it's sluggish but stable, increase stiffness.
+        "hip_pitch": DCMotorCfg(
+            joint_names_expr=[".*pitch"],
+            stiffness=140.0,
+            damping=5.0,
+            effort_limit=18.0,
+            saturation_effort=23.7,
+            velocity_limit=30.0,
+            armature = 0.002,  # kg·m²
+        ),
+
+        "knee": DCMotorCfg(
+            joint_names_expr=[".*knee"],
+            stiffness=180.0,
+            damping=6.0,
+            effort_limit=18.0,
+            saturation_effort=23.7,
+            velocity_limit=30.0,
+            armature = 0.002,  # kg·m²
+        ),
+
+        "ankle": DCMotorCfg(
+            joint_names_expr=[".*ankle"],
+            stiffness=90.0,
+            damping=3.0,
+            effort_limit=18.0,
+            saturation_effort=23.7,
+            velocity_limit=30.0,
+            armature = 0.002,  # kg·m²
         )
+
+
     }
 )
